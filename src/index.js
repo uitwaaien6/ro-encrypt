a50be9671bcd02e692a249982f2eebc22e4962bf
Ruzgar Ata Ozkan <ruzgarata6@gmail.com>
2020-12-16 03:08:32 +0300

    dotenv added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 const { createRepresentationEncryptionObject, encryptPassword, decryptPassword } = require('./encryption/representationalDatabaseEncryption');
 
 const representationalEncryptionObject = createRepresentationEncryptionObject(3);
 
-const encryptedPassword = encryptPassword(representationalEncryptionObject, 'erw342');
+const encryptedPassword = encryptPassword(representationalEncryptionObject, process.env.PASSWORD);
 
-console.log(decryptPassword(representationalEncryptionObject, encryptedPassword));
+decryptPassword(representationalEncryptionObject, encryptedPassword);
