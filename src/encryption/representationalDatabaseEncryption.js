abb7e630b6d66287f8ffc6da6f309387b6e9db70
Ruzgar Ata Ozkan <ruzgarata6@gmail.com>
2020-12-28 21:36:59 +0300

    casual

--- src/encryption/representationalDatabaseEncryption.js
+++ src/encryption/representationalDatabaseEncryption.js
@@ -7,133 +7,140 @@ class RDE {
     static createKey(password) {
 
         // console.log(` ~ Initalizing KEY OBJECT...`);
-    
-        const encryptionLetters = '12345678901234567890qwertyuiopasdfghjklzxcvbnm'.toUpperCase().split('');
-    
-        const sectionNameEncryptionLetters = 'qwertyuiopasdfghjklzxcvbnm'.toUpperCase().split('');
-    
-        const letters = password.split('');
 
-        const complexity = 3;
-    
-        let createdSections = [];
-    
-        for (let i = 0; i < complexity * 3; i++) {
-    
-            let sectionNameEncryption = '';
-            
-            for (let j = 0; j < complexity * 4; j++) {
+        if (password) {
+
+            const encryptionLetters = '12345678901234567890qwertyuiopasdfghjklzxcvbnm'.toUpperCase().split('');
+            const sectionNameEncryptionLetters = 'qwertyuiopasdfghjklzxcvbnm'.toUpperCase().split('');
+            const letters = password.split('');
+            const complexity = 3;
+        
+            let createdSections = [];
+        
+            for (let i = 0; i < complexity * 3; i++) {
+        
+                let sectionNameEncryption = '';
                 
-                const randomSectionNameEncryptionIndex = Math.floor(Math.random() * sectionNameEncryptionLetters.length);
-                sectionNameEncryption += sectionNameEncryptionLetters[randomSectionNameEncryptionIndex];
-                
+                for (let j = 0; j < complexity * 4; j++) {
+                    
+                    const randomSectionNameEncryptionIndex = Math.floor(Math.random() * sectionNameEncryptionLetters.length);
+                    sectionNameEncryption += sectionNameEncryptionLetters[randomSectionNameEncryptionIndex];
+                    
+                }
+        
+                for (let j = 0; j < complexity; j++) { 
+        
+                    while (sectionNameEncryption === createdSections[j]) {
+                        sectionNameEncryption = '';
+        
+                        for (let k = 0; k < complexity * 4; k++) {
+                            const randomSectionNameEncryptionIndex = Math.floor(Math.random() * sectionNameEncryptionLetters.length);
+                            sectionNameEncryption += sectionNameEncryptionLetters[randomSectionNameEncryptionIndex];
+                        }
+                    }
+                    
+                }
+        
+                createdSections = createdSections.concat(sectionNameEncryption);
             }
-    
-            for (let j = 0; j < complexity; j++) { 
-    
-                while (sectionNameEncryption === createdSections[j]) {
-                    sectionNameEncryption = '';
-    
-                    for (let k = 0; k < complexity * 4; k++) {
-                        const randomSectionNameEncryptionIndex = Math.floor(Math.random() * sectionNameEncryptionLetters.length);
-                        sectionNameEncryption += sectionNameEncryptionLetters[randomSectionNameEncryptionIndex];
+        
+            const sections = [...createdSections];
+        
+            const keyObject = {};
+        
+            if (complexity <= 2 || complexity >= 5) {
+                for (let i = 0; i < letters.length; i++) {
+                    keyObject[letters[i]] = {};
+        
+                    for (let j = 0; j < sections.length; j++) {
+        
+                        keyObject[letters[i]][sections[j]] = [];
                     }
                 }
-                
+        
+                console.error(chalk.red(' ! Complexity argument of the createKey cannot be belov 2 or above 4'));
+                console.log(' ~ Creating an empty Key Object...');
+                console.log(chalk.yellow(' ! Recommended complexity number is 4 or above, default complexity number is set to 3'));
+                return null;
             }
-    
-            createdSections = createdSections.concat(sectionNameEncryption);
-        }
-    
-        const sections = [...createdSections];
-    
-        const keyObject = {};
-    
-        if (complexity <= 2 || complexity >= 5) {
+        
             for (let i = 0; i < letters.length; i++) {
-                keyObject[letters[i]] = {};
-    
-                for (let j = 0; j < sections.length; j++) {
-    
-                    keyObject[letters[i]][sections[j]] = [];
-                }
-            }
-    
-            console.error(chalk.red(' ! Complexity argument of the createKey cannot be belov 2 or above 4'));
-            console.log(' ~ Creating an empty Key Object...');
-            console.log(chalk.yellow(' ! Recommended complexity number is 4 or above, default complexity number is set to 3'));
-            return null;
-        }
-    
-        for (let i = 0; i < letters.length; i++) {
-    
-            const letter = letters[i];
-            keyObject[letter] = {};
-    
-            for (let j = 0; j < sections.length; j++) { // j represents the section configuration, configure a specific section in the belove code.
-    
-    
-                const section = sections[j];
-                keyObject[letter][section] = [];
-    
-                for (let k = 0; k < complexity * 2; k++) { // k represents how many elements should a section have, example: sectionName: ['SDF', 'SDF', 'SDFD'];
-    
-                    let encryption = '';
-    
-                    for (let l = 0; l < complexity * 2; l++) {
-                        const randomEncryptionLetter = Math.floor(Math.random() * encryptionLetters.length);
-                        encryption = encryption + encryptionLetters[randomEncryptionLetter];
-                    }
-    
-                    // in here we are iterating through all the sections index in all the properties of the object to avoid the same encryption in the object properties, doesnt matter if we get same encryption in the same properties array but we change it due to syntax
-                    // but if we get same encryption in different properties of the representationObject password will be decrypted wrongly.
-                    // because it will take the first property letter of the representaionObject when it is matched with the encryptedPassword so
-                    // all the properties has to have unique encryptions in it.
-                    if (keyObject[letter] && keyObject[letter][section]) {
-                        const keyObjectProperties = Object.getOwnPropertyNames(keyObject);
-    
-                        for (let m = 0; m < keyObjectProperties.length; m++) {
-    
-                            for (let n = 0; n < sections.length; n++) {
-    
-                                for (let sectionsIndex = 0; sectionsIndex < 4; sectionsIndex++) {
-    
-                                    if (keyObject[letters[m]][sections[n]]) {
-                                        while (encryption === keyObject[letters[m]][sections[n]][sectionsIndex]) {
-                                            // development purposes
-                                            console.log(` ~ Same Encryption Found in: `, chalk.yellow(letters[m]));
-                                            console.log(` ~ Section: `, chalk.yellow(sections[n]));
-                                            console.log(` ~ Array:`, keyObject[letters[m]][sections[0]], ` Index:`, chalk.yellow(sectionsIndex));
-                                            
-                                            encryption = '';
-                                            for (let l = 0; l < complexity; l++) {
-                                                const randomEncryptionLetter = Math.floor(Math.random() * encryptionLetters.length);
-                                                encryption = encryption + encryptionLetters[randomEncryptionLetter];
+        
+                const letter = letters[i];
+                keyObject[letter] = {};
+        
+                for (let j = 0; j < sections.length; j++) { // j represents the section configuration, configure a specific section in the belove code.
+        
+        
+                    const section = sections[j];
+                    keyObject[letter][section] = [];
+        
+                    for (let k = 0; k < complexity * 2; k++) { // k represents how many elements should a section have, example: sectionName: ['SDF', 'SDF', 'SDFD'];
+        
+                        let encryption = '';
+        
+                        for (let l = 0; l < complexity * 2; l++) {
+                            const randomEncryptionLetter = Math.floor(Math.random() * encryptionLetters.length);
+                            encryption = encryption + encryptionLetters[randomEncryptionLetter];
+                        }
+        
+                        // in here we are iterating through all the sections index in all the properties of the object to avoid the same encryption in the object properties, doesnt matter if we get same encryption in the same properties array but we change it due to syntax
+                        // but if we get same encryption in different properties of the representationObject password will be decrypted wrongly.
+                        // because it will take the first property letter of the representaionObject when it is matched with the encryptedPassword so
+                        // all the properties has to have unique encryptions in it.
+                        if (keyObject[letter] && keyObject[letter][section]) {
+                            const keyObjectProperties = Object.getOwnPropertyNames(keyObject);
+        
+                            for (let m = 0; m < keyObjectProperties.length; m++) {
+        
+                                for (let n = 0; n < sections.length; n++) {
+        
+                                    for (let sectionsIndex = 0; sectionsIndex < 4; sectionsIndex++) {
+        
+                                        if (keyObject[letters[m]][sections[n]]) {
+                                            while (encryption === keyObject[letters[m]][sections[n]][sectionsIndex]) {
+                                                // development purposes
+                                                console.log(` ~ Same Encryption Found in: `, chalk.yellow(letters[m]));
+                                                console.log(` ~ Section: `, chalk.yellow(sections[n]));
+                                                console.log(` ~ Array:`, keyObject[letters[m]][sections[0]], ` Index:`, chalk.yellow(sectionsIndex));
+                                                
+                                                encryption = '';
+                                                for (let l = 0; l < complexity; l++) {
+                                                    const randomEncryptionLetter = Math.floor(Math.random() * encryptionLetters.length);
+                                                    encryption = encryption + encryptionLetters[randomEncryptionLetter];
+                                                }
                                             }
                                         }
                                     }
                                 }
                             }
                         }
+        
+                        keyObject[letter][section] = keyObject[letter][section].concat(encryption);
+                        encryption = '';
                     }
-    
-                    keyObject[letter][section] = keyObject[letter][section].concat(encryption);
-                    encryption = '';
                 }
             }
-        }
-
-        // shuffle the properties of the keyObject to prevent the sequences of UTF-16 code units values attacks
-        // efffsane oldu lul
-        const shuffledKeyObjectProperties = Object.getOwnPropertyNames(keyObject).sort(() => Math.random() - 0.5);
-        const shuffledKeyObject = {};
-
-        // bind the properties of the keyObject to shuffledKeyObject
-        shuffledKeyObjectProperties.forEach((item, index) => {
-            shuffledKeyObject[item] = keyObject[item];
-        });
     
-        return Object.freeze({ ...shuffledKeyObject });
+            // shuffle the properties of the keyObject to prevent the sequences of UTF-16 code units values attacks
+            // efffsane oldu lul
+            const shuffledKeyObjectProperties = Object.getOwnPropertyNames(keyObject).sort(() => Math.random() - 0.5);
+            const shuffledKeyObject = {};
+    
+            // bind the properties of the keyObject to shuffledKeyObject
+            shuffledKeyObjectProperties.forEach((item, index) => {
+                shuffledKeyObject[item] = keyObject[item];
+            });
+        
+            return Object.freeze({ ...shuffledKeyObject });
+
+        } else {
+            console.log(chalk.red(' ! Password is not provided in the createKey'));
+            console.log(chalk.red(' ! Returnin null..'));
+            return null;
+        }
+    
+
     }
 
     static encrypt(password, keyObject) {
@@ -189,7 +196,8 @@ class RDE {
         
             return `${encryptedPassword}`; 
         } else {
-            console.error(chalk.red(' ! keyObject or password couldnt be found'));
+            console.error(chalk.red(' ! keyObject or password is not provided in encrypt function'));
+            console.error(chalk.red(' ! Returning null...'));
             return null;
         }
     
@@ -243,7 +251,8 @@ class RDE {
     
             return decryptedPassword;
         } else {
-            console.error(chalk.red(' ! keyObject or encrypted password couldnt be found'));
+            console.error(chalk.red(' ! keyObject or encrypted password is not provided in decrypt function'));
+            console.error(chalk.red(' ! Returning null...'));
             return null;
         }
     
